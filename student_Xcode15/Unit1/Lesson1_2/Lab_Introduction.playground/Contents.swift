f64ed83f27400262942e96d24696b31992997b1e
Reagan  Anderson <rakkimonthly345@gmail.com>
2024-10-22 12:05:16 -0400

    Playground

--- student_Xcode15/Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -12,11 +12,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Sleeper (song title), by BBNG feat Charlotte Wilson (artist)")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("Heard she was a sleeper")
+print("Glad you got a dreamer")
+print("Does the sun still greet you?")
+print("Her shadow's eager")
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
