911d2bc6ffd70e0f67683579a258758835f4a82c
Reagan  Anderson <rakkimonthly345@gmail.com>
2024-10-25 20:38:55 -0400

    Lab 1-5 Completed

--- student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -10,7 +10,16 @@
 let targetLowerBound = 120
 let targetUpperBound = 150
 let currentHR = 147
-
+let isInTarget = 150
+let isBelowTarget = currentHR < targetLowerBound 
+let isAboveTarget = currentHR > targetUpperBound
+if currentHR == isInTarget {
+    print ("You're right on track!")
+} else if currentHR == isBelowTarget{
+    print ("You're doing great, but try to push it a bit!")
+} else if currenthr == isAboveTarget {
+    print ("You're on fire! Slow it down just a bit.")
+}
 /*:
 [Previous](@previous)  |  page 5 of 9  |  [Next: Exercise - Switch Statements](@next)
  */
--- student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -8,9 +8,9 @@ let number2 = 25
 
 var largest: Int
 if number1 > number2 {
-    largest = number1
-} else {
     largest = number2
+} else {
+    largest = number1
 }
 /*:
 [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
