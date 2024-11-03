6fa719d7a338b484c8191ef798c49e88604e4bef
Reagan  Anderson <rakkimonthly345@gmail.com>
2024-11-03 12:28:56 -0500

    FirstTimeDebugging complete

--- student_Xcode15/Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
+++ student_Xcode15/Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -2,29 +2,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad( {
+    func viewDidLoad( {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var sample = "sample"
+        let sample = "sample"
         print(sample)
         
-        if false {
-            print("Will this line of code ever be reached?")
-            someMetho()
+        func someMethod() {
+            
+            if false {
+                print("Will this line of code ever be reached?")
+                someMethod()
+            }
         }
-        
         
     }
     
-    func someMethod() {
-
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
