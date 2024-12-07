2cff6a818f0680b246e0e797d639840d0b7763e2
Reagan  Anderson <rakkimonthly345@gmail.com>
2024-12-07 15:50:59 -0500

    Finished

---
+++ student_Xcode15/Unit2/Basic Interactions/Basic Interactions/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Basic Interactions
+//
+//  Created by Reagan Anderson on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet var label: UILabel!
+
+    @IBOutlet var textField: UITextField!
+  
+    @IBAction func setButtonTapped(_ sender: UIButton) {
+        
+        print(label!)
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        print (<#T##items: Any...##Any#>)
+    }
+    
+}
+
+
